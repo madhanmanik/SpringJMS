afb251ca6943ca9602f775014c0e1598a88117e8
madhanmanik <madhanmanik@gmail.com>
2018-11-30 16:48:57 +0530

    JMS

---
+++ src/main/java/com/uae/SpringBootJMSApplication.java
@@ -0,0 +1,11 @@
+package com.uae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;  
+
+@SpringBootApplication 
+public class SpringBootJMSApplication {  
+    public static void main(String[] args) {  
+        SpringApplication.run(SpringBootJMSApplication.class, args);  
+    }  
+}  
